e4b20aa3f3ee2be410f4c7413195f2dd2f4bdf53
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-05-20 23:02:57 +0530

    using direct command

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 import time
 from threading import Thread
 
-# BackupCommand=[
-#   "echo",
-#   "2"
-# ]
-# command=[
-#   "echo",
-#   "1"
-# ]
 # AUDIO="1-7nyfzwu9monpNry6l2qyEJkVIMN6kQ-"
 # VIDEO="1-DFjo8aLalapsI1np168JiXjzK7HWw0Q"
 VIDEO=str(os.environ['VIDEO'])
 AUDIO=str(os.environ['AUDIO'])
-command=str(os.environ['cmd']).split() 
-BackupCommand=str(os.environ['bcmd']).split() 
+# command=str(os.environ['cmd']).split() 
+# BackupCommand=str(os.environ['bcmd']).split() 
+command=['ffmpeg', '-i', './media/backup/a.mp3', '-re', '-stream_loop', '-1', '-i', './media/bg1.mp4', '-vf', 'scale=-1:720', '-shortest', '-strict', '-2', '-c:v', 'libx264', '-preset', 'ultrafast', '-crf', '28', '-threads', '8', '-c:a', 'aac', '-b:v', '1500k', '-b:a', '192k', '-pix_fmt', 'yuv420p', '-r', '24', '-x264-params', 'keyint=36:min-keyint=24:scenecut=-1', '-shortest', '-f', 'flv', 'rtmp://a.rtmp.youtube.com/live2/mm56-xc3k-3dq5-5jfj-c9ac']
+BackupCommand=['ffmpeg', '-i', './media/a.mp3', '-re', '-stream_loop', '-1', '-i', './media/bg1.mp4', '-vf', 'scale=-1:720', '-shortest', '-strict', '-2', '-c:v', 'libx264', '-preset', 'ultrafast', '-crf', '28', '-threads', '8', '-c:a', 'aac', '-b:v', '1500k', '-b:a', '192k', '-pix_fmt', 'yuv420p', '-r', '24', '-x264-params', 'keyint=36:min-keyint=24:scenecut=-1', '-shortest', '-f', 'flv', 'rtmp://a.rtmp.youtube.com/live2/mm56-xc3k-3dq5-5jfj-c9ac']
+
 print(command)
 print(BackupCommand)
 settingUp=True
@@ -25,7 +20,7 @@ settingUp=True
 def setup(): 
   global settingUp
   gdd.download_file_from_google_drive(file_id=AUDIO,dest_path='./media/a.mp3')
-  ##gdd.download_file_from_google_drive(file_id=VIDEO,dest_path='./media/bg1.mp4')
+  #gdd.download_file_from_google_drive(file_id=VIDEO,dest_path='./media/bg1.mp4')
   settingUp=False
 
 def main():
