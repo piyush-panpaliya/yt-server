36fb8dc50f9f47d3933089728804cb792f2d96bc
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-05-20 22:21:45 +0530

    initial

---
+++ main.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+from google_drive_downloader import GoogleDriveDownloader as gdd
+import time
+from threading import Thread
+
+# BackupCommand=[
+#   "echo",
+#   "2"
+# ]
+# command=[
+#   "echo",
+#   "1"
+# ]
+# AUDIO="1-7nyfzwu9monpNry6l2qyEJkVIMN6kQ-"
+# VIDEO="1-DFjo8aLalapsI1np168JiXjzK7HWw0Q"
+VIDEO=str(os.environ['VIDEO'])
+AUDIO=str(os.environ['AUDIO'])
+command=str(os.environ['cmd']).split() 
+BackupCommand=str(os.environ['bcmd']).split() 
+
+settingUp=True
+
+def setup(): 
+  global settingUp
+  gdd.download_file_from_google_drive(file_id=AUDIO,dest_path='./media/a.mp3')
+  ##gdd.download_file_from_google_drive(file_id=VIDEO,dest_path='./media/bg1.mp4')
+  settingUp=False
+
+def main():
+  thread = Thread(target = setup, args = ())
+  thread.start()
+
+  while settingUp:
+    time.sleep(1)
+    subprocess.run(BackupCommand,shell=True)
+
+  subprocess.run(command,shell=True)
+
+main()
